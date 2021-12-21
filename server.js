624d05e9f5c9808503d75c09b91bc22619d59a70
Brian <BMArsenault@gmail.com>
2021-12-21 00:11:43 -0500

    Added parties table

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ const db = mysql.createConnection(
   {
     host: 'localhost',
     // Your MySQL username,
-    user: '',
+    user: 'root',
     // Your MySQL password
-    password: '',
+    password: 'TooCloseToSee#311',
     database: 'election'
   },
   console.log('Connected to the election database.')
